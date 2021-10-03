655bb2cfb9d57e3ea65de84400f2ef6e06a2cc11
Oristic16 <78655050+Oristic16@users.noreply.github.com>
2021-10-04 01:46:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,14 +3,13 @@ if(isset($_GET['controller'])&&isset($_GET['action']))
 {   $controll = $_GET['controller'];
     $action = $_GET['action'];
 }else
-{
-    $controll = 'pages';
+{   $controll = 'pages';
     $action = 'home';
 }?>
 <html>
 <head></head>
 <body>
-    <?php echo "controller =".$controller.",action = ".$action;?>
+    <?php echo "controller = ".$controller.", action = ".$action;?>
     <br>[<a href="">Home </a>] <br>
     <?php require_once("routes.php");?>
 </body>
