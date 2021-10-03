02bcf6442fe94cbabd1916d2e26261ebaddd7d2f
krape1690 <91468514+krape1690@users.noreply.github.com>
2021-10-04 01:14:20 +0700

    Create routes.php

---
+++ routes.php
@@ -0,0 +1,16 @@
+<?php
+$controller = array('pages'=>['home','error']);
+
+function call($controller, $action){
+        echo "routes to".$controller."-".$action."<br>";
+}
+
+if(array_key_exists($controller,$controllers))
+{   if(in_array($action,$controllers[$controller]))
+    {       call($controller,$action);      }
+    else
+    {       call('pages','error');          }
+}
+else
+{   call('pages','error');  }
+?>
